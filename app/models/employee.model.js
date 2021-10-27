f5974a1f5811b2f5428270afabe16b6e2c43a033
varadharjm <varadharaj@biovustech.com>
2021-10-27 17:59:38 +0530

    non

--- app/models/employee.model.js
+++ app/models/employee.model.js
@@ -3,7 +3,7 @@ const mongooes = require("mongoose");
 const employeeModel = mongooes.Schema({
     first_name :{
         type:String,
-        min:3,
-        maxlength
+        required: true,
+         maxLength: 20
     }
 })
